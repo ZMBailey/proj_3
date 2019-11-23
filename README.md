73d3f6ee5e6330f107f55b0c9dd3d574434fb4e8
Zander Bailey <zbailey656@gmail.com>
2019-11-22 22:57:18 -0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# proj_3
-Music Classification
+# Understanding
+A lot depends on the genre of a music and categorizing music into genres takes time. It takes human listening time, and while it is sometimes enojoyable, human listening may not be practical for a large number of songs. It may be possible to save human-hours by having a machine learning model analyze the songs for us! 
+
+# Data
+Approximately 100 songs were collected from 6 genres, Classical, Electronic, Jazz, Country, Metal and HipHop. Each song was broken up into 20 sec intervals to increase the size of the data set. These audio files were then processed using Librosa to extract sonic features that can help identify a songs genre. For an introduction into the music information retreival techniques, please see the related blog:
+https://medium.com/@patrickbfuller/librosa-a-python-audio-libary-60014eeaccfb.
+
+# Model
+To classify the genres of the songs, a Support Vector Machine classifier was trained on vectors of sonic features present in the songs. Before training an 80 20 train test split was performed.
+
+Based on sonic features, model predicts the probability that the set of features belongs to each of the 6 genres. A random guess would have a 17% chance of being right and our model predicted genre correctly 81% of the time.
+
+To analyze a song from a youtube a custom class 'listens' to the song by extracting the features from the song using librosa and then passes those features into the trained model.
+
--- SongClassifier.py
+++ SongClassifier.py
@@ -19,21 +19,24 @@ class SongClassifier():
         'Classical']
     
     def __init__(self):
-        df = pd.read_csv('data_3k.csv')
-        genre_map = {g:i for i, g in enumerate(df.label.unique())}
-        #df = df[df.label != 'Classical']
-        X = df.drop(['name','label'],axis=1)
-        X['beats'] = X['beats'].astype(float)
-        y = df['label'].map(genre_map)
-        X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=20)
-        self.scaler = StandardScaler()
-        self.scaler.fit(X_train)
-        X_train_scaled = self.scaler.transform(X_train)
+        filepath1 = "model.sav"
+        filepath2 = 'scaler.sav'
+        self.classifier = pickle.load(open(filepath1, 'rb')) 
+        self.scaler = pickle.load(open(filepath2, 'rb'))
+        # df = pd.read_csv('data_3k.csv')
+        # genre_map = {g:i for i, g in enumerate(df.label.unique())}
+        # X = df.drop(['name','label'],axis=1)
+        # X['beats'] = X['beats'].astype(float)
+        # y = df['label'].map(genre_map)
+        # X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=20)
+        # self.scaler = StandardScaler()
+        # self.scaler.fit(X_train)
+        # X_train_scaled = self.scaler.transform(X_train)
         
-        self.classifier = SVC(C=25,probability=True,decision_function_shape=('ovo'))
-        self.classifier.fit(X_train_scaled,y_train)
+        # self.classifier = SVC(C=25,probability=True,decision_function_shape=('ovo'))
+        # self.classifier.fit(X_train_scaled,y_train)
         
-        self.genres = [i for i in df.label.unique()]
+        # self.genres = [i for i in df.label.unique()]
         #print(self.genres)
     
     
@@ -67,7 +70,7 @@ class SongClassifier():
 
         songname = 'mystery_song.mp3'
         y_total, sr = librosa.load(songname, mono=True, duration=180, sr=None)
-        for i, y in enumerate(np.array_split(y_total, 6)):
+        for y in np.array_split(y_total, 6):
             tempo, beats = librosa.beat.beat_track(y=y, sr=sr)
             chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
             rmse = librosa.feature.rmse(y=y)
@@ -90,9 +93,9 @@ class SongClassifier():
                 seg_dict['mfcc_' + str(j+1)] = np.mean(e)
 
             song_list.append(seg_dict)
-            
+        print("test")
         song_df = pd.DataFrame(song_list,columns=header)
-        song_df['beats'] = song_df['beats'].astype(float)
+        song_df.beats = song_df['beats'].astype(float)
         self.song = song_df
         self.classify()
         #return song_df
